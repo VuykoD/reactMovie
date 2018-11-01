8c56a89c08c25d440fd7c57ee2b57d2b083375ab
dimon <vuyko@ukr.net>
2018-11-01 19:17:48 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+/* eslint-disable react/no-multi-comp */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 import Loader from 'react-loader-spinner';
 import './App.css';
@@ -7,199 +9,186 @@ class App extends Component {
   constructor(props, ctx) {
     super(props, ctx);
     this.state = {
-      renderLoader:false,
+      renderLoader: false,
       popular: null,
-      inputValue:''
-    }
+    };
   }
-
 
   componentDidMount() {
     this.takeFirstData();
   }
 
-  takeFirstData(){
-    this.setState ({
-      renderLoader:true,
-    })
-    axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=03b96ac61ca771f70dc412e781c3361e&language=en-US`)
+  fetchMovies = (query) => {
+    this.setState({
+      renderLoader: true,
+    });
+
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=03b96ac61ca771f70dc412e781c3361e&language=en-US&page=1&include_adult=false&query=${query}`)
       .then((response) => {
-          this.genres=response;
+        setTimeout(() => this.setState({
+          popular: response,
+          renderLoader: false,
+        }), 500);
       })
-      .catch(function (error) {
-        //console.log(error);
+      .catch((error) => {
+        // console.log(error);
+      });
+  };
+
+  takeFirstData() {
+    this.setState({
+      renderLoader: true,
+    });
+    axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=03b96ac61ca771f70dc412e781c3361e&language=en-US')
+      .then((response) => {
+        this.genres = response;
+      })
+      .catch((error) => {
+        // console.log(error);
       });
 
-    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=03b96ac61ca771f70dc412e781c3361e&language=en-US&page=1`)
+    axios.get('https://api.themoviedb.org/3/movie/popular?api_key=03b96ac61ca771f70dc412e781c3361e&language=en-US&page=1')
       .then((response) => {
-        setTimeout(()=>
-        this.setState ({
-          popular:response,
-          renderLoader:false,
-        }), 500)
+        setTimeout(() => this.setState({
+          popular: response,
+          renderLoader: false,
+        }), 500);
       })
-      .catch(function (error) {
-        //console.log(error);
+      .catch((error) => {
+        // console.log(error);
       });
   }
 
-  fetchMovies=(query)=>{
-    this.setState ({
-      renderLoader:true,
-    })
-
-    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=03b96ac61ca771f70dc412e781c3361e&language=en-US&page=1&include_adult=false&query=${query}`)
-    .then((response) => {
-      setTimeout(()=>
-        this.setState ({
-          popular:response,
-          renderLoader:false,
-        }), 500)
-    })
-    .catch(function (error) {
-      //console.log(error);
-    });
-  };
-
   render() {
-
+    const loader = this.state.renderLoader;
     return (
       <div className="App">
         <Search
           fetchMovies={this.fetchMovies}
         />
-
-        {(this.state.renderLoader) &&
-          <Loader type="ThreeDots" color="#somecolor" height={80} width={40} />
+        {loader
+          && <Loader type="ThreeDots" color="#somecolor" height={80} width={40} />
         }
-        <p/>
-        {(this.state.popular && !this.state.renderLoader)&&
+        {(this.state.popular && !this.state.renderLoader) &&
           <div>
-
             <List
               list={this.state.popular}
               genres={this.genres}
-              >
-              </List>
+            />
           </div>
         }
-
-
       </div>
     );
   }
 }
 
 class Search extends Component {
+  static propTypes = {
+    fetchMovies: PropTypes.func,
+  };
+
   constructor(props, ctx) {
     super(props, ctx);
     this.state = {
-      inputValue:''
-    }
+      inputValue: '',
+    };
   }
 
-
-  onChange(e){
+  onChange(e) {
     this.setState({
-      inputValue:e.target.value
-    })
-    this.props.fetchMovies(e.target.value)
+      inputValue: e.target.value,
+    });
+    this.props.fetchMovies(e.target.value);
   }
 
-  render(){
+  render() {
     return (
       <input
         value={this.state.inputValue}
-        onChange={(e)=>this.onChange(e)}
-        />
-    )
+        onChange={(e) => this.onChange(e)}
+      />
+    );
   }
 }
 
 class List extends Component {
+  static propTypes = {
+    genres: PropTypes.shape({}),
+    list: PropTypes.shape({
+      data: PropTypes.shape({
+        total_results: PropTypes.number,
+        results: PropTypes.array,
+      }),
+    }),
+  };
 
-  render(){
-    return(
-
+  render() {
+    return (
       <div>
         {(this.props.list.data) &&
           <div>
-
-            <span>Всего фильмов - {this.props.list.data.total_results}</span>
-            {this.props.list.data.results.map((movie, index) =>
-              <div key={movie.id}>
-                <MovieCard
-                  movie={movie}
-                  genres={this.props.genres}
-                />
-
-
-              </div>
-            )}
+            <span>
+              {'Всего фильмов - '}
+              {this.props.list.data.total_results}
+            </span>
+            {this.props.list.data.results.map((movie) => (
+              <MovieCard
+                key={movie.id}
+                movie={movie}
+                genres={this.props.genres}
+              />))
+            }
           </div>
         }
       </div>
-    )
+    );
   }
 }
 
 class MovieCard extends Component {
-  constructor(props, ctx) {
-    super(props, ctx);
-    this.state = {
-      image_path:''
-    }
-  }
+  static propTypes = {
+    movie: PropTypes.shape({}),
+    genres: PropTypes.shape({
+      data: PropTypes.shape({
+        genres: PropTypes.array,
+      }),
+    }),
+  };
 
-  genres(genre_ids, index){
+  genres(genre_ids, index) {
     this.genreName(genre_ids);
-    return(
+    return (
       <span key={index}>
-         {this.genre},{" "}
+        {this.genre}
+        {', '}
       </span>
-    )
+    );
   }
 
-  genreName(genre_ids){
-    const genre=this.props.genres.data.genres;
+  genreName(genre_ids) {
+    const genre = this.props.genres.data.genres;
 
-    for (let i = 0; i < genre.length; i++ ){
-      if (genre[i].id === genre_ids) return this.genre=genre[i].name
+    for (let i = 0; i < genre.length; i++) {
+      if (genre[i].id === genre_ids) return this.genre = genre[i].name;
     }
-
-  }
-
-  getImages(id){
-
-    axios.get(`https://api.themoviedb.org/3/movie/`+id+`/images?api_key=03b96ac61ca771f70dc412e781c3361e&height=720`)
-      .then((response) => {
-        this.setState({
-          image_path:response.data.posters[0].file_path
-        });
-      })
-      .catch(function (error) {
-
-      });
   }
 
   render() {
-    const movie=this.props.movie;
-    this.getImages(movie.id)
+    const movie = this.props.movie;
+
     return (
       <div>
-        { this.state.image_path &&
-          <img
-            src={`https://image.tmdb.org/t/p/w185${this.state.image_path}`}
-            alt={movie.title}
-          />
-        }
         <p>
-          {movie.title} -{" "}
-          {movie.genre_ids.map((genre_ids, index)=>this.genres(genre_ids, index))}
+          {movie.title}
+          {' - '}
+          {movie.genre_ids.map((genre_ids, index) => this.genres(genre_ids, index))}
         </p>
+        <img
+          src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
+          alt={movie.title}
+        />
       </div>
-
-    )
+    );
   }
 }
 
